47fd586e66cf632f564a4ae41dc0bf3c658809cb
varghesejoseph <varghesejoseph854@gmail.com>
2024-05-08 10:54:35 +0530

    calculator

---
+++ prgm4.js
@@ -0,0 +1,22 @@
+function display(num){
+    result.value += num
+}
+function CLEARALL() {
+    result.value=""
+    
+}
+function back() {
+    result.value=result.value.slice(0,-1)
+}
+
+function equal(){
+    try{
+        result.value=eval(result.value)
+    }
+    catch(error){
+        result.value='error'
+        setTimeout(()=>{result.value=""},1000)
+    }
+
+    
+}
